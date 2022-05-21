3530b09e25924c582eefae1b9502e3f82e8e0f68
CasperDoesCoding <casper.soderback@live.vasteras.se>
2022-05-21 18:58:01 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -10,15 +10,15 @@ def main():
     threading.Thread(target=user_interface.main_display).start()
 
 def convert_old_data_structure_to_new():
-    if not os.path.exists("./account_data.json"):
+    if not os.path.exists("./src/account_data.json"):
         return
     
-    with open("account_data.json", "r") as file:
+    with open("./src/account_data.json", "r") as file:
         data = json.load(file)
         for key, account in data.items():
             new_account = accounts.Account(summoner_username=account["summoner_name"], region=accounts.ServerRegion[str.upper(account["region"])], username=account["login_name"], password=account["password"])
 
-    os.remove("./account_data.json")
+    os.remove("./src/account_data.json")
 
 if __name__ == "__main__":
     convert_old_data_structure_to_new()
